de6b30d645cd3a4818586295776aba02b849efd4
Morgan Bergen <morganmahabergen@icloud.com>
2022-03-06 17:21:08 -0600

    inserted the write up, which will act as the readme fle

--- lab04/main.py
+++ lab04/main.py
@@ -1,8 +1,13 @@
 
-
-
 def main():
-        print("tetsing 1,2,3")
+        print("lab04")
 
 if __name__ == "__main__":
         main()
+
+
+'''
+1. create and test an implementation of a LinkedList
+2.  make an implementation of the Browser, using your LinkedList
+
+'''
