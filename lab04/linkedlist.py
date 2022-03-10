377bebe1a2409dee20ee26699a431e97673eb2a3
Morgan Bergen <morganmahabergen@icloud.com>
2022-03-10 11:14:34 -0600

    implementing insert memeber method
    if index == 0
    if index == self._length
    else:

--- lab04/linkedlist.py
+++ lab04/linkedlist.py
@@ -5,15 +5,39 @@ class LinkedList:
 	
 	def __init__(self):
 		self._front = None
+		self._back = None
 		self._length = 0
 		
 	def length(self):
 		jumper = self._front
-		length = 0
+		self._length = 0
 		while jumper != None:
 			jumper = jumper.next
-			length = length + 1
-		if length == self._length:
-			return(self._length)
+			self._length = self._length + 1
+		return(self._length)
 	
-	
+	'''
+	Insert the entry at the index. Valid indices range from 0 to length inclusively.
+	Inserting at index = 0 inserts at the front.
+	Inserting at entry index=length adds to the back.
+	Each insert increases the length by 1.
+	'''
+	def insert(self, index, entry):
+		valid = (index >= 0) and (index <= self._length)
+		if valid:
+			new_node = Node(entry)
+			if index == 0:
+				self._front = new_node
+				self._front.next = None
+			elif (index == self._length):
+				self._back.next = new_node
+				self._back = new_node
+			
+			else:
+				print()
+				
+			
+			
+		else:
+			raise IndexError(f"IndexError: index {index} is out of range.")
+		
--- lab04/main.py
+++ lab04/main.py
@@ -4,6 +4,14 @@ from linkedlist import LinkedList
 def main():
 	list = LinkedList()
 	print(list.length())
+	
+	try:
+		list.insert(0, "http://google.com")
+	except IndexError as e:
+		print(e)
+		
+	print(list._front.data)
+	print(list._front.next)
 
 if __name__ == "__main__":
 	main()
