b8535d51356d7add830468baf591ef85e6f42c64
Morgan Bergen <morganmahabergen@icloud.com>
2022-03-11 12:13:14 -0600

    Completed the final

--- web-history-simulator/browser.py
+++ web-history-simulator/browser.py
@@ -5,53 +5,46 @@ from linkedlist import LinkedList
 class Browser:
 	
 	def __init__(self, file):
-		self.web = LinkedList() #self.web_history rename after youre done
+		self.web_history = LinkedList() #self.web_history_history rename after youre done
 		self.current = 0
 		self.file = file
-		
-		
+	
+	
 	def navigate_to(self, url):
-		if self.web.is_empty():
-			self.web.insert(0, url)
+		if self.web_history.is_empty():
+			self.web_history.insert(0, url)
 			self.current = 0
 		
-		elif (self.web.get_length() - 1) == self.current:
-			self.web.insert(self.web.get_length(), url)
-			self.current = self.web.get_length() - 1
-		# length = 6, current = 2
-		# remove index 3, 4, 5
-		# position = 3
-		# remove(3)
-		# remove (4)
-		# remove(5)
+		elif (self.web_history.get_length() - 1) == self.current:
+			self.web_history.insert(self.web_history.get_length(), url)
+			self.current = self.web_history.get_length() - 1
 		else:
-			position = self.current + 1
-			for position in range(self.web.get_length()):
-				self.web.remove(position)
-		# current = 2
-		# current = 2 + 1
-		# insert (3, url)
+			for x in reversed(range(self.current+1, self.web_history.get_length())):
+				self.web_history.remove(x)
 			self.current = self.current + 1
-			self.web.insert(self.current, url)
-		self.history()
-			
+			self.web_history.insert(self.current, url)
+	
 	def back(self):
+		if self.current != 0:
+			self.current = self.current - 1
+	
 	def forward(self):
+		if self.current != self.web_history.get_length() - 1:
+			self.current = self.current + 1
+	
+	
 	def history(self):
 		print("Oldest")
 		print("===========")
-		for i in range self.web.get_length():
+		for i in range(self.web_history.get_length()):
 			if i == self.current:
-				print(f"{i} {self.web.get_entry(i)} <== current {self.current}")
+				
+				print(f"{self.web_history.get_entry(i)}", end="")
+				print(f" <== current")
 			else:
-				print(f"{i} {self.web.get_entry(i)}")
+				print(self.web_history.get_entry(i))
 		print("===========")
 		print("Newest")
-		
-	def tester(self):
-		self.web.navigate
-	
-	
 	
 	def fileIO(self):
 		
@@ -60,34 +53,15 @@ class Browser:
 		for line in in_file:
 			command = line.split()
 			if command[0] == "NAVIGATE":
-				self.navigate(command[0], command[1])
-				
-			elif command[0] == "HISTORY":
-				self.print(command[0])
-				
-			elif command[0] == "BACK":
-				self.back(command[0])
-				
-			elif command[0] == "FORWARD":
-				self.forward(command[0])
+				self.navigate_to(command[1])
 			
+			elif command[0] == "HISTORY":
+				self.history()
+			
+			elif command[0] == "BACK":
+				self.back()
+			
+			elif command[0] == "FORWARD":
+				self.forward()
+	
 		in_file.close()
-	
-
-
-'''
-history.get_length()
-history.is_empty()
-history.get_entry(index, data)
-histroy.clear()
-
-
-raises index errors
-	history.get_node_at(index)
-	history.insert(index, data)
-	history.get_entry(index)
-	
-raises runtime error and index error
-	histroy.remove(index)
-	
-'''
--- web-history-simulator/main.py
+++ web-history-simulator/main.py
@@ -3,7 +3,7 @@ from browser import Browser
 
 def main():
 	exec = Browser("input.txt")
-	
+	exec.fileIO()
 
 if __name__ == "__main__":
 	main()
