9b164bbb65c1d0d3bf9d61c65c9ed87805a9d995
Morgan Bergen <morganmahabergen@icloud.com>
2022-03-10 22:26:08 -0600

    reinitated the executive functiionality

--- web-history-simulator/browser.py
+++ web-history-simulator/browser.py
@@ -7,13 +7,62 @@ class Browser:
 	def __init__(self, file):
 		self.history = LinkedList()
 		self.file = file
+		self.current = -1
 		
+	def navigate(self, command, url):
+		self.current = self.current + 1
+		if self.history.is_empty():
+			self.history.insert(self.history.get_length(), url)
+		else:
+			self.history.insert(self.history.get_length(), url)
+		
+		
+	def print(self, command):
+		if self.history.is_empty():
+			print("Oldest")
+			print(self.current)
+			print("===========")
+			print("===========")
+			print("Newest")
+		else:
+			print("Oldest")
+			print("===========")
+			for i in range(self.history.get_length()):
+				if i == self.current:
+					print(f"{self.history.get_entry(i)} <==current {self.current}")
+				else:
+					print(self.history.get_entry(i))
+			
+			print("===========")
+			print("Newest")
+		
+	def back(self, command):
+		self.current = self.current - 1
+		
+		
+	def forward(self, command):
+		self.current = self.current + 1
+		print(self.current)
+	
 	def fileIO(self):
-		return(self.file)
 		
-	
-	
-	
+		in_file = open(self.file)
+		
+		for line in in_file:
+			command = line.split()
+			if command[0] == "NAVIGATE":
+				self.navigate(command[0], command[1])
+				
+			elif command[0] == "HISTORY":
+				self.print(command[0])
+				
+			elif command[0] == "BACK":
+				self.back(command[0])
+				
+			elif command[0] == "FORWARD":
+				self.forward(command[0])
+			
+		in_file.close()
 	
 
 
--- web-history-simulator/main.py
+++ web-history-simulator/main.py
@@ -1,4 +1,9 @@
 
 from browser import Browser
 
+def main():
+	exec = Browser("input.txt")
+	exec.fileIO()
 
+if __name__ == "__main__":
+	main()
