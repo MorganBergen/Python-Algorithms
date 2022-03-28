57ff1e9602fb738aa3697319ac13ac95516e8a1f
Morgan Bergen <morganmahabergen@icloud.com>
2022-03-28 16:16:30 -0500

    Generated Boiler plate for exercise1
    exercise1 == exponential.py

--- recursion/exponential.py
+++ recursion/exponential.py
@@ -1,3 +1,12 @@
 '''
-testing
+@file		exponential.py
+@author		Morgan Bergen
+@date		March 28 2022
+@brief		Exercise 1:  Recursive Power Function
+			Create a program that takes in a base and exponent for the user. If the input is valid, it prints the answer of the base taken to the power given. You must require the user to give ints for both the base and the exponent. Also, the exponent must be zero or larger.
 '''
+
+def main():
+	print()
+	
+main()
