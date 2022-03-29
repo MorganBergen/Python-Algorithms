3f91b775ced300561bd70cb32c7cf278d22172d6
Morgan Bergen <morganmahabergen@icloud.com>
2022-03-29 10:52:06 -0500

    FInalized

--- recursion/outbreak.py
+++ recursion/outbreak.py
@@ -3,29 +3,19 @@
 @author		Morgan Bergen
 @date		March 28 2022
 @brief		Exercise 2:  Outbreak Returns
-
 			Flu season is upon us and the number of people getting sick is growing.
-
 			On day 1, there was only 6 people had the flu
-On day 2, it jumped to 20.
-On day 3, there were 75
+			On day 2, it jumped to 20.
+			On day 3, there were 75
+			Every day since, the number of people who have the flu is equal to the last 3 days combined
+			You will make a program that will ask the user for what day they want a count of people with the flu for. Then display the amount. You must use recursion to solve the calculation of infected for a given day. Assume the user will input a valid day.
 
-Every day since, the number of people who have the flu is equal to the last 3 days combined
-You will make a program that will ask the user for what day they want a count of people with the flu for. Then display the amount. You must use recursion to solve the calculation of infected for a given day. Assume the user will input a valid day.
-
-d = days
-day	people
-1 = 6
-2 = 20
-3 = 75
-4 = 75 		+ 20	  + 6
-4 = (4 - 1) + (4 - 2) + (4 - 3)
-d = (d - 1) + (d - 2) + (d - 3)
-recursive case
-
+day 1 = 6 people
+day 2 = 20 people
+day 3 = 75 people
+day 4 = ((4 - 1) people + (4 - 2) people + (4 - 3) people)
+day n = ((n - 1) + (n - 2) + (n - 3)) people
 '''
- 
-
 
 def main():
 	print("OUTBREAK!")
