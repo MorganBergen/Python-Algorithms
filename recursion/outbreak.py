63c6dd6c08d8a2b4f310748d7f965a1724a24f4e
Morgan Bergen <morganmahabergen@icloud.com>
2022-03-29 10:49:09 -0500

    Update

--- recursion/outbreak.py
+++ recursion/outbreak.py
@@ -55,6 +55,28 @@ main()
 
 
 
+'''
+
+Performed base file system operations from the Linux Command Line Enviroment when implementing programs.
+Derived and employed basic algoritms from given problems
+Demonstrated basic programming concepts and constructs such as for/while/do-while loops, primative data types i.e. long, short, int, double, strings, char, arrays, pointers, variables, function scope, general debugging techniques, structures, classes, compliation and makefile techniques.
+Basic functionalities of git
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
