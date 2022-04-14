5d0b71a49bb8ec17a0e472858f80476492453abd
Morgan Bergen <morganmahabergen@icloud.com>
2022-04-14 13:33:45 -0500

    fileIO
    Try Catch Block without break statement.
    Running while loop.
    For a Break Statement:  It immediately exits the closest enclosing while or for loop
    statement, skipping its associated else (if any).

--- blob-game/source/main.py
+++ blob-game/source/main.py
@@ -11,33 +11,31 @@ def main():
 
 def fileIO(file):
 	
-	works = False
-	
-	
-	
-	try:
-#		stream = open(file, 'r')
-		fo = open(file, 'r')
-		decrepid = open("fatal.txt", 'r')
-	except FileNotFoundError as e:
-		print(e)
-	finally:
-		print("name of file is ", fo.name)
-		dimensions = fo.readline().split()
-		start_dimensions = fo.readline().split()
-		numRows = dimensions[0]
-		numCols = dimensions[1]
-		startRow = start_dimensions[0]
-		startCol = start_dimensions[1]
-		map = fo.read().splitlines()
-		print(f"startRow = {startRow}")
-		print(f"startCol = {startCol}")
-		print(f"numRows = {numRows}")
-		print(f"numCols = {numCols}")
-		print(map)
-		fo.close()
-		print("closed ", fo.closed)
-
+	run = True
+	while run:
+		try:
+			fo = open(file, 'r')
+		except FileNotFoundError as e:
+			print(e)
+		finally:
+			run = False
+			print("name of file is ", fo.name)
+			dimensions = fo.readline().split()
+			start_dimensions = fo.readline().split()
+			numRows = dimensions[0]
+			numCols = dimensions[1]
+			startRow = start_dimensions[0]
+			startCol = start_dimensions[1]
+			map = fo.read().splitlines()
+			print(f"startRow = {startRow}")
+			print(f"startCol = {startCol}")
+			print(f"numRows = {numRows}")
+			print(f"numCols = {numCols}")
+			print(map)
+			fo.close()
+			print("closed ", fo.closed)
+	else:
+		print("we have hit our else statement")
 
 
 main()
