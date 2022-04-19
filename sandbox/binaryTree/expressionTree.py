20bdd57aaca78b40cf7d3793ef7ac246d7866550
Morgan Bergen <morganmahabergen@icloud.com>
2022-04-18 21:19:34 -0500

    updated

--- sandbox/binaryTree/expressionTree.py
+++ sandbox/binaryTree/expressionTree.py
@@ -1,3 +1,13 @@
 class ExpressionTree:
+	# builds an expression tree for the expression string.
+	def __init__(self, expStr):
+		self._expTree = None
+		self._buildTree(expStr)
 	
-	
+	# evaluates the expression tree and returns the resulting value.
+	def evaluate(self, varMap):
+		return self._evalTree(self._expTree, varMap)
+
+	# returns a string representation of the expression tree.
+	def __str__(self):
+		return self._buildString(self._expTree)
