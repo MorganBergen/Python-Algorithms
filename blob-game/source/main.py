523a81baa52a3d83ee62d844db5b6e3d17c0ad3e
Morgan Bergen <morganmahabergen@icloud.com>
2022-04-24 15:51:01 -0500

    dOxygen commenting has been implemented

--- blob-game/source/main.py
+++ blob-game/source/main.py
@@ -1,12 +1,31 @@
+'''
+@file		main.py
+@author		Morgan Bergen
+@date		April 24 2022
+@brief		This file contains the main functon 
+			The main function is in charge of 
+			- calling upon constructing and initilaizing the Maze class.
+			- reading in the characters of the input file.
+			- running the path for the blob to spread.
+			- revealing the final output.
+'''
+
 
 from maze import Maze
 
-
+'''
+@pre		None
+@post		builds ADT Maze and prints out final output
+@param		None
+@raises		None
+@returns	None
+'''
 def main():
-	m = Maze()
-	m.build_grid()
-	m.start()
-	m.print_final()
+
+	exec = Maze()
+	exec.build_grid()
+	exec.start()
+	exec.print_final()
 
 if __name__ == "__main__":
 	main()
--- blob-game/source/maze.py
+++ blob-game/source/maze.py
@@ -1,6 +1,28 @@
+'''
+@file		maze.py
+@author		Morgan Bergen
+@date		April 24 2022
+@brief		This file contains all of the member variables and member methods implementations of the project.
+			This Maze class does the following at a high level overview of it's job
+			- it will request file input from the user
+			- read in the characters and initialize the grid within a list of lists
+			- determine if the file is valid or not
+			- it will enumerate the number of sewers, people, total rows, total cols, start row and start col indexes.
+			- the walk function will then use recursion for the blob to spread
+				- helper methods such as mark and valid_spot are used to determine whether the blob should move or not
+			- print_final will reveal to the user in a grid view the finalized blob in the city outcome
+			
+'''
 
 class Maze:
 	
+	'''
+	@pre		Maze() must be called outside this class
+	@post		Member variables are initialized at their default values
+	@param		None
+	@raises		None
+	@returns	None
+	'''
 	def __init__(self):
 		self.file = ""
 		self.total_rows = 0
@@ -16,12 +38,26 @@ class Maze:
 		self.track = 0
 		self.cur = []
 
+	'''
+	@pre		build_grid() must be called
+	@post		total_sewers is accurately initialized based on how many occurances of '@' are found from the input file
+	@param		None
+	@raises		None
+	@returns	None
+	'''
 	def count_sewers(self):
 		for row in self.grid:
 			for value in row:
 				if value == '@':
 					self.total_sewers = self.total_sewers + 1
-	
+
+	'''
+	@pre		build_grid() must be called
+	@post		list variables sewer_row_locations and sewer_col_locations are accurately initialized based on the index position of '@'
+	@param		None
+	@raises		None
+	@returns	None
+	'''	
 	def locate_sewers(self):
 		for i in range(len(self.grid)):
 			for j in range(len(self.grid[i])):
@@ -29,18 +65,41 @@ class Maze:
 					self.sewer_row_locations.append(i)
 					self.sewer_col_locations.append(j)
 
+	'''
+	@pre		start() must have already been called
+	@post		prints out the grid, plus each characters corresponding index row and column position
+	@param		None
+	@raises		None
+	@returns	None
+	'''
 	def print_grid(self):
 		for i in range(len(self.grid)):
 			for j in range(len(self.grid[i])):
 				print(f" [{self.grid[i][j]}]{i}{j} ", end="")
 			print()
  
+	'''
+	@pre		start() must have already been called
+	@post		prints out the grid
+	@param		None
+	@raises		None
+	@returns	None
+	'''
 	def print_simple(self):
 		for i in range(len(self.grid)):
 			for j in range(len(self.grid[i])):
 				print(f"{self.grid[i][j]}", end="")
 			print()
 
+
+
+	'''
+	@pre		start() must have already been called
+	@post		calls print_simple() and prints important member variable values
+	@param		None
+	@raises		None
+	@returns	None
+	'''
 	def print_info(self):
 		print("__________grid__________")
 		self.print_simple()
@@ -53,10 +112,32 @@ class Maze:
 			print(f"[{self.sewer_row_locations[i]}, {self.sewer_col_locations[i]}] ", end="")
 		print("\n________________________________")
 
+	'''
+	@pre		start() must have already been called
+	@post		prints the write up's preferred output of the grid
+	@param		None
+	@raises		None
+	@returns	None
+	'''
 	def print_final(self):
-		self.print_simple()
+		for i in range(len(self.grid)):
+			for j in range(len(self.grid[i])):
+				for k in range(len(self.sewer_row_locations)):
+					if i == self.sewer_row_locations[k] and j == self.sewer_col_locations[k]:
+						self.grid[i][j] = '@'
+				print(f"{self.grid[i][j]}", end="")
+			print()
 		print(f"total eaten {self.total_eaten}")		
 
+	'''
+	@pre		Maze() must have already been constructed
+	@post		initializes the file member variable from requesting input from the user
+				if fileio works properly then count_pp(), count_sewers(), and locate_sewers() will all be called for initializing
+				important information regarding the characters of the input file.
+	@param		None
+	@raises		None
+	@returns	None
+	'''
 	def build_grid(self):
 		x = False
 		while x == False:
@@ -66,6 +147,20 @@ class Maze:
 		self.sewers = self.count_sewers()
 		self.locate_sewers()
 
+	'''
+	@pre		build_grid() must have allready been called
+	@post		the input file is read in
+				grid, total_rows, total_cols, start_row, start_col will all be initialized based on the file
+				valid_dimenions() will be called in order to determine if the input file is legal
+				- we assume the file is of the following format type
+					<numRows> <numCols>
+					<Blob startRow> <Blob startCol>
+					<map characters>
+				- FileNotFoundError will display a message indicates that the file is not located within current project directory
+	@param		None
+	@raises		FileNotFoundError
+	@returns	True if the fileIO worked, False if otherwise
+	'''
 	def fileio(self):
 		try:
 			f = open(self.file, 'r')
@@ -89,6 +184,23 @@ class Maze:
 		f.close()
 		return True
 
+	'''
+	@pre		fileio() must have already been called
+	@post		will determine if the dimensions of the file are accurate
+				we assume the map characters match the parameters given from the file
+	@param		None
+	@raises		None
+	@returns	True if the dimensions are valid
+				False if the dimensions provided in the input file are invalid 
+				- and displaying an error messaging indicating which dimension was invalid
+				
+				conditions for invaid dimensions:
+				- if file doesn't exist
+				- if numRows are less than 1
+				- if numCols are less than 1
+				- if start position is not within range
+
+	'''
 	def valid_dimensions(self):
 		if self.total_rows < 1:
 			print(f"error: invalid dimensions provided, total rows cannot be less than 1")
@@ -111,18 +223,53 @@ class Maze:
 		else:
 			return True
 	
+	'''
+	@pre		None
+	@post		will re-read the file again and reinitialize grid character and member variable values
+	@param		None
+	@raises		None
+	@returns	None
+	'''
 	def reset_grid(self):
 		self.fileio()
 
+	'''
+	@pre		build_grid() must have been called and fileio() must have returned True
+	@post		counts the amount of people there are in the grid and returns that integer value
+	@param		None
+	@raises		None
+	@returns	ppl
+	'''
 	def count_ppl(self):
 		ppl = 0
 		for i in range(len(self.grid)):
 			ppl = self.grid[i].count('P') + ppl
 		return (ppl)
 
+	'''
+	@pre		build_grid() must have been called correctly
+	@post		The blob spreads through the grid
+	@param		None
+	@raises		None
+	@returns	walk(start_row, start_col)
+	'''
 	def start(self):
-		self.walk(self.start_row, self.start_col)
+		return (self.walk(self.start_row, self.start_col))
 
+	'''
+	@pre		start() must have been called
+	@post		the blob will have moved throughout the grid
+				self.cur is marked with the current index position that the walk is analyzing
+				mark(row, col) will alter the grid's values based on the location of the grid
+				track is used to track how many moves the blob took in order to recurse through the maze
+				if the blob is allowed to move a recursive call will allow for the grid to be marked
+					- valid_spot will check in the following order: up, right, down, left
+					- if valid_spot() returns true then a recursive call in the direction will be made
+				if the blob cannot move up, right, down, or left then the grid at the current position will be marked as 'B'
+	@param		row, col
+	@raises		None
+	@returns	None
+	'''
 	def walk(self, row, col):
 		
 		# print(f" [{self.grid[row][col]}]{self.cur[0]}{self.cur[1]}")
@@ -143,8 +290,25 @@ class Maze:
 			self.walk(row, col - 1)	
 		
 		self.grid[row][col] = 'B'
-		
 
+	'''
+	@pre		walk() must have been called
+	@post		streets represented by capital 'S' 
+				- so if the grid[row][col] is 'S' then it's marked as a integer 
+				  which represents the current total number of moves the blob took in order to recurse throughout the maze
+				people represented by capital 'P'
+				- so if the grid[row][col] is 'P' then the grid is marked by track member variable
+				- total eaten will be increamented by one
+				buildings are represented by '#' 
+				- so the blob cannot mark this position
+				sewers are represented by '@'
+				- all sewers are considered adjacent
+				- if the blob has eaten all possible people and runs into a sewer, then the sewer will be marked as track
+				- else then the sewer will be marked as it was prior '@'
+	@param		None
+	@raises		None
+	@returns	None if it can mark on a valid character, False if otherwise
+	'''
 	def mark(self, row, col):
 		
 		if self.grid[row][col] == 'P':
@@ -160,6 +324,18 @@ class Maze:
 		else:
 			return False
 
+	'''
+	@pre		walk(row, col) must have already been called
+	@post		checks if the blob can move
+				if it encounters a '@' aka a sewer space and 
+				 - it has tried to spread in all other directions, 
+				 - it will spread into the sewer, which allows it to move to all connected sewer spaces one at a time
+				 - once the Blob moves through a sewer it continues to move as normal
+				 - walk(row, col) will be called based on the next located sewer space
+	@param		row, col		
+	@raises		None
+	@returns	True if the move is valid, false if otherwise
+	'''
 	def valid_spot(self, row, col):
 		if row >= 0 and row < self.total_rows and col >= 0 and col < self.total_cols:
 			if self.grid[row][col] == 'P':
