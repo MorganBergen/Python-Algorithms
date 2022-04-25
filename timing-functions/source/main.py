5a11d2c2aac83006dc7be7c1d9acbdee73f5d65c
Morgan Bergen <morganmahabergen@icloud.com>
2022-04-25 15:23:06 -0500

    stack directory has been added

---
+++ timing-functions/source/main.py
@@ -0,0 +1,37 @@
+'''
+in this lab we will time various methods from data structures we've made so far save those times
+plot them to see if they match the theoretical expectations
+'''
+
+import time
+
+def nanosec_to_sec(ns):
+	billion = 1000000000 # one thousand million
+	return (ns/billion)
+
+def main():
+	print("beginning the timing code...")
+	num_iterations = 100000000 # one hundred million
+	start_time = time.process_time_ns()
+
+	# loop that does nothing but repeat, hence the _ variable and the keywork pass
+	# which just means do nothing
+
+	for _ in range(num_iterations):
+		pass
+
+	end_time = time.process_time_ns()
+	
+	print(f"total time in nano seconds: ", end_time-start_time)
+	print(f"total time in seconds: ", nanosec_to_sec(end_time-start_time))
+
+if __name__ == "__main__":
+	main()
+
+
+
+
+
+
+
+
---
+++ timing-functions/source/stack/stack.py
@@ -0,0 +1,31 @@
+
+from Node import node
+
+class Stack:
+
+	def __init__(self):
+		self.top = None
+	
+	def push(self, data):
+		return None
+		
+	def pop(self):
+		return None
+	
+	def peak(self):
+		return None
+		
+	def is_empty(self):
+		return None
+		
+
+
+
+
+
+
+
+
+
+
+
