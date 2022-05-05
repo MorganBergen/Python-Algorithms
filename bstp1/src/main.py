a7dc8639f0919abf3ec2238f8a9039e336340868
Morgan Bergen <morganmahabergen@icloud.com>
2022-05-05 17:24:18 -0500

    still got it

--- bstp1/src/main.py
+++ bstp1/src/main.py
@@ -2,6 +2,8 @@
 from tree import Tree
 from node import Node
 
+#include <iostream>
+
 def main():
 	
 	tree = Tree()
@@ -14,12 +16,21 @@ def main():
 	
 	
 	print("did we add a new node?")
+	
 	state = tree.add(4, "NEW DATA")
 	if state == False:
 		print(f"{tree.root.left.left.key} is {tree.root.left.left.data} ")
 	else:
 		print("error")
 	
+	next = tree.add(3, "new node")
+	
+	if next == True:
+		print(f"{tree.root.left.left.right.key} is {tree.root.left.left.right.data} ")
+	else:
+		print("error")
+		
+	std::cout << "hello my name is morgan" << std::endl;
 
 
 if __name__ == "__main__":
--- bstp1/src/tree.py
+++ bstp1/src/tree.py
@@ -35,45 +35,32 @@ class Tree:
 		else:
 			return (node.data)
 		
-	def build(self):
-		self.root = Node(60, "60 root")
-		self.size += 1
-		self.root.left = Node(12, "60 root lc")
-		self.size += 1
-		self.root.right = Node(90, "60 root rc")
-		self.size += 1
-		self.root.left.left = Node(4, "12s lc")
-		self.size += 1
-		self.root.left.right = Node(41, "12s rc")
-		self.size += 1
-		self.root.left.left.left = Node(1, "4s lc")
-		self.size += 1
-		self.root.left.right.left = Node(29, "41s lc")
-		self.size += 1
-		self.root.left.right.left.left = (23, "29s lc")
-		self.size += 1
-		self.root.left.right.left.right = (37, "29s rc")
-		self.size += 1
-		self.root.right.left = Node(71, "90s lc")
-		self.size += 1
-		self.root.right.right = Node(84, "90s rc")
-		self.size += 1
-		self.root.right.right = Node(100, "90s rc")
-		self.size += 1
 	
 	# adds a new entry to the tree or replaces the value of an existing one
 	def add(self, key, data):
 		# find the ndoe containing the key, if it exists
 		node = self._search(self.root, key)
 		
-		#if the key is already in the tree, update its value
+		#if the key is already in the tree, updates its value
 		if node != None:
 			node.data = data
 			return False
 		
 		# otherwise add a new entry
-		
-		
+		else:
+			self.root = self._insert(self.root, key, data)
+			self.size +=
+			return True
+			
+	
+	def _insert(self, subtree, key, data):
+		if subtree == None:
+			subtree = Node(key, data)
+		elif key < subtree.key:
+			subtree.left = self._insert(subtree.left, key, data)
+		elif key > subtree.key:
+			subtree.right = self._insert(subtree.right, key, data)
+		return subtree
 	
 	def _search(self, subtree, target_key):
 		
@@ -112,9 +99,37 @@ class Tree:
 			return subtree
 		else:
 			return (self._max(subtree.right))
+			
+	def build(self):
+		self.root = Node(60, "60 root")
+		self.size += 1
+		self.root.left = Node(12, "60 root lc")
+		self.size += 1
+		self.root.right = Node(90, "60 root rc")
+		self.size += 1
+		self.root.left.left = Node(4, "12s lc")
+		self.size += 1
+		self.root.left.right = Node(41, "12s rc")
+		self.size += 1
+		self.root.left.left.left = Node(1, "4s lc")
+		self.size += 1
+		self.root.left.right.left = Node(29, "41s lc")
+		self.size += 1
+		self.root.left.right.left.left = (23, "29s lc")
+		self.size += 1
+		self.root.left.right.left.right = (37, "29s rc")
+		self.size += 1
+		self.root.right.left = Node(71, "90s lc")
+		self.size += 1
+		self.root.right.right = Node(84, "90s rc")
+		self.size += 1
+		self.root.right.right = Node(100, "90s rc")
+		self.size += 1
+	
 
 
 '''
+
 
 
 	def inorder(subtree)
